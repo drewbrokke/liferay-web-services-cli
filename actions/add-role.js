50262ba0d0a7e7dbb6872e4677960ffd482cf84b
drewbrokke <drew.brokke@liferay.com>
2015-03-28 10:39:24 -0700

    Use new structure for ar (add role) command

--- actions/add-role.js
+++ actions/add-role.js
@@ -1,12 +1,6 @@
-var superb = require('superb');
 var Action = require('../actionClass');
 
-module.exports = function(program, callback) {
-	var type = (0 < program.type < 4) ? program.type : 1;
-	var typeLabel = getTypeLabel(type);
-
-	var name = program.rolenamename || (superb().toUpperCase() + typeLabel);
-
+module.exports = function(name, type, callback) {
 	var role = {
 		name: name,
 		titleMap: JSON.stringify({
@@ -17,21 +11,6 @@ module.exports = function(program, callback) {
 		}),
 		type: type
 	};
-
-	function getTypeLabel(type) {
-		if (type === 1) {
-			return " Regular Role";
-		}
-		else if (type === 2) {
-			return " Site Role";
-		}
-		else if (type === 3) {
-			return " Organization Role";
-		}
-		else {
-			return " Role";
-		}
-	}
 
 	var action = new Action('/role/add-role', role);
 
