2fc8ba5e8cd0b1c376d05302103b96d770620abb
drewbrokke <drew.brokke@liferay.com>
2015-03-28 12:44:14 -0700

    Use new structure for aur (add user role) command

--- actions/add-user-role.js
+++ actions/add-user-role.js
@@ -3,7 +3,7 @@ var Action = require('../actionClass');
 module.exports = function(userId, roleIds, callback) {
 	var userRole = {
 		userId: userId,
-		roleIds: roleIds
+		roleIds: JSON.stringify(roleIds)
 	};
 
 	var action = new Action('/role/add-user-roles', userRole);
