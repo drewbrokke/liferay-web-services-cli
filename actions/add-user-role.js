61571b8289c706f5367d2216c764f60fe9a475ec
drewbrokke <drew.brokke@liferay.com>
2015-03-28 11:12:25 -0700

    Simplify add-user-role action

--- actions/add-user-role.js
+++ actions/add-user-role.js
@@ -1,35 +1,13 @@
-var request = require('request');
 var Action = require('../actionClass');
 
-module.exports = function(program, callback) {
-	request.post(
-		{
-			// Verify roleId
-			url: 'http://test@liferay.com:test@localhost:8080/api/jsonws/role/get-role',
-			form: {
-				roleId: program.role
-			}
-		},
-		function(error, response, body) {
-			if (!error && (response.statusCode === 200)) {
-				console.log('Valid role id. Adding role to user...');
+module.exports = function(userId, roleIds, callback) {
+	var userRole = {
+		userId: userId,
+		roleIds: roleIds
+	};
 
-				var roleIds = [];
-				roleIds.push(program.role);
+	var action = new Action('/role/add-user-roles', userRole);
 
-				var userRole = {
-					userId: program.user,
-					roleIds: JSON.stringify(roleIds)
-				};
+	action.doAction(callback);
 
-				var action = new Action('/role/add-user-roles', userRole);
-
-				action.doAction(callback);
-			}
-			else {
-				console.log('Cannot send request.');
-				console.log('Not a valid Role ID.');
-			}
-		}
-	);
 }
