2298a7759322634435b16e16fff13ea1eaa3f819
drewbrokke <drew.brokke@liferay.com>
2015-03-30 16:11:14 -0700

    'gu' command (get-users) will now return all users in company if called with no arguments

---
+++ actions/get-company-users.js
@@ -0,0 +1,13 @@
+var Action = require('../actionClass');
+
+module.exports = function(callback) {
+	var payload = {
+		companyId: 20152,
+		start: 0,
+		end: 999999
+	}
+
+	var action = new Action('/user/get-company-users', payload);
+
+	action.doAction(callback);
+}
