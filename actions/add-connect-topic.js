eb6b9c302905aec7d2f8e71bf0e0b7f370836d82
Evan Thibodeau <evan.thibodeau@liferay.com>
2015-05-04 15:34:47 -0700

    Fix Topics - Name cannot have spaces and the description needs to be escaped and have the number of characters limited.

--- actions/add-connect-topic.js
+++ actions/add-connect-topic.js
@@ -1,7 +1,11 @@
 var Action = require('../actionClassConnect');
 
 module.exports = function(payload, callback) {
-	var url = 'name=' + payload.name + '&description=' + payload.description;
+	var name = payload.name.replace(/\s/g, '');
+
+	var description = encodeURIComponent(payload.description.slice(0, 200));
+
+	var url = 'p_p_lifecycle=1&name=' + name + '&description=' + description;
 
 	var action = new Action('/connect/topics/add?', url);
 
